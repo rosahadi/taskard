848474561d041a2f7383c9fc7666380b185be992
rosa <rosahadi19@gmail.com>
2025-02-10 14:30:33 +0300

    feat(server): add Zod schemas for update password

--- server/src/utils/auth-validations.ts
+++ server/src/utils/auth-validations.ts
@@ -21,4 +21,40 @@ export const LoginSchema = z.object({
   email: z.string().email({ message: 'Invalid email format' }),
   password: z.string().min(1, { message: 'Incorrect email or password' }),
 });
-feat(validation): add Zod schemas for forgot password and reset password
+
+export const ForgotPasswordSchema = z.object({
+  email: z.string().email({ message: 'Invalid email format' }),
+});
+
+export const ResetPasswordSchema = z.object({
+  password: z
+    .string()
+    .min(10, { message: 'Password must be at least 10 characters long' })
+    .regex(/[A-Z]/, {
+      message: 'Password must contain at least one uppercase letter',
+    })
+    .regex(/[a-z]/, {
+      message: 'Password must contain at least one lowercase letter',
+    })
+    .regex(/[\W_]/, {
+      message: 'Password must contain at least one special character',
+    }),
+});
+
+export const UpdatePasswordSchema = z.object({
+  currentPassword: z
+    .string()
+    .min(1, { message: 'Current password is required' }),
+  password: z
+    .string()
+    .min(10, { message: 'Password must be at least 10 characters long' })
+    .regex(/[A-Z]/, {
+      message: 'Password must contain at least one uppercase letter',
+    })
+    .regex(/[a-z]/, {
+      message: 'Password must contain at least one lowercase letter',
+    })
+    .regex(/[\W_]/, {
+      message: 'Password must contain at least one special character',
+    }),
+});
